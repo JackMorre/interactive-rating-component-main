72bf08118e2d121ada15cdcb22c2bbc19ebba50c
Jack Morrell <jack.morrell79@googlemail.com>
2022-12-06 23:02:13 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// get all elements needed for the best effect
+
+const numbers = document.querySelectorAll('.number-style')
+const rating = document.querySelector('#rating')
+const final = document.querySelector('#final')
+const button = document.querySelector('.button')
+const noRatingText = document.querySelector('#no-rating')
+const selectedNumber = document.querySelector('.selected-number')
+
+//get number for numbers array to be able to reference and manipulate later
+let arrNumber = 0
+
+// selection of number
+numbers.forEach(function(e) {
+    e.addEventListener('click', function(e) {
+        // remove previous clicked
+        if(numbers[arrNumber].classList.contains('clicked') === true) {
+            numbers[arrNumber].classList.remove('clicked')
+        }
+        // this change arrnumber to suitable number for array (as they have the number in the text i decided to us this and then just take away to match what i need for the array)
+        arrNumber = e.target.innerText - 1
+        // add back the class of clicked to make it highlighted
+        e.target.classList.add('clicked') 
+        noRatingText.classList.add('hidden')
+
+    })
+})
+
+// button event
+button.addEventListener('click', function() {
+    // making sure that we can submit without giving a rating and also a prompt
+    if(numbers[arrNumber].classList.contains('clicked') !== true) {
+        noRatingText.classList.remove('hidden')
+    } 
+    else {
+        // changes the number on final page to be what the rating was.
+        selectedNumber.textContent = arrNumber + 1
+
+        // switches two views
+        rating.classList.add('hidden')
+        final.classList.remove('hidden')
+    }
+})
+
+
+
+
