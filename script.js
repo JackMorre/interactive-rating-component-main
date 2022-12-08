4eb03829fc06c923ddf4bedbffdbd796804cd0bb
Jack Morrell <jack.morrell79@googlemail.com>
2022-12-08 14:31:22 +0100

    Change input in html, changed JS for this

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ const selectedNumber = document.querySelector('.selected-number')
 let arrNumber = 0
 
 // selection of number
-numbers.forEach(function(e) {
-    e.addEventListener('click', function(e) {
+numbers.forEach(function(event) {
+    event.addEventListener('click', function(e) {
+        e.preventDefault()
         // remove previous clicked
         if(numbers[arrNumber].classList.contains('clicked') === true) {
             numbers[arrNumber].classList.remove('clicked')
@@ -42,6 +43,40 @@ button.addEventListener('click', function() {
     }
 })
 
+// orriginal code
 
+// let arrNumber = 0
+
+// // selection of number
+// numbers.forEach(function(e) {
+//     e.addEventListener('click', function(e) {
+//         // remove previous clicked
+//         if(numbers[arrNumber].classList.contains('clicked') === true) {
+//             numbers[arrNumber].classList.remove('clicked')
+//         }
+//         // this change arrnumber to suitable number for array (as they have the number in the text i decided to us this and then just take away to match what i need for the array)
+//         arrNumber = e.target.innerText - 1
+//         // add back the class of clicked to make it highlighted
+//         e.target.classList.add('clicked') 
+//         noRatingText.classList.add('hidden')
+
+//     })
+// })
+
+// // button event
+// button.addEventListener('click', function() {
+//     // making sure that we can submit without giving a rating and also a prompt
+//     if(numbers[arrNumber].classList.contains('clicked') !== true) {
+//         noRatingText.classList.remove('hidden')
+//     } 
+//     else {
+//         // changes the number on final page to be what the rating was.
+//         selectedNumber.textContent = arrNumber + 1
+
+//         // switches two views
+//         rating.classList.add('hidden')
+//         final.classList.remove('hidden')
+//     }
+// })
 
 
